20d4f25b5845cdbe6c38378d104744eb1ba9d296
Noah Pederson <pedersonnoah@gmail.com>
2015-01-23 23:06:52 -0600

    Added navigation drawer

--- app/src/main/java/com/animebracket/android/MainActivity.java
+++ app/src/main/java/com/animebracket/android/MainActivity.java
@@ -1,12 +1,19 @@
 package com.animebracket.android;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.animebracket.android.Util.Constants;
@@ -19,12 +26,22 @@ import com.animebracket.android.fragments.StartupFragment;
 import com.google.gson.Gson;
 
 
-public class MainActivity extends Activity implements JsonStringCallback, LoginFragment.LoginFragmentCallback {
+public class MainActivity extends ActionBarActivity implements JsonStringCallback, LoginFragment.LoginFragmentCallback {
+
+    private String[] navBarTextItems;
+    private ListView navBarListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navBarTextItems = getResources().getStringArray(R.array.nav_drawer_buttons_text);
+        navBarListView = (ListView) findViewById(R.id.nav_bar_list_view);
+
+        //Set up the navbar
+        navBarListView.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, navBarTextItems));
+        navBarListView.setOnItemClickListener(new DrawerItemClickListener());
 
         // Initialize singleton cookie store
         CookieSyncManager.createInstance(this); //Deprecated in API 21, but because we're supporting
@@ -61,6 +78,18 @@ public class MainActivity extends Activity implements JsonStringCallback, LoginF
     }
 
     @Override
+    public void onBackPressed() {
+        //If the current fragment is a login fragment, call that fragments onBackPressed method
+        //If not, call the default onBackPressed method
+        try {
+            LoginFragment cFragment = (LoginFragment) getFragmentManager().findFragmentById(R.id.container);
+            cFragment.onBackPressed();
+        } catch (ClassCastException ex) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -85,11 +114,16 @@ public class MainActivity extends Activity implements JsonStringCallback, LoginF
 
     @Override
     public void onJsonStringReceived(String jsonString) {
+        //If the string is null, it means the device couldn't reach the animebracket servers
+        if(jsonString == null) {
+            Log.d(Constants.JSON, "Unable to connect to animebracket.com");
+            //TODO: Show dialogue box
+        }
         UserInfo userInfo = null;
         try {
             Gson gson = new Gson();
             userInfo = gson.fromJson(jsonString, UserInfo.class);
-            Log.d(Constants.JSON, "Loaded json from http request");
+            Log.d(Constants.JSON, "Loaded json from http request: " + jsonString);
         } catch (Exception e) {}
 
         //If userInfo is null, you aren't logged in and it should show the login fragment
@@ -112,5 +146,19 @@ public class MainActivity extends Activity implements JsonStringCallback, LoginF
         getFragmentManager().beginTransaction()
                 .replace(R.id.container, new RunningBracketsFragment())
                 .commit();
+    }
+
+    //Click listener for the navigation drawer
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+
+    private void selectItem(int position) {
+        //TODO: Switch fragments when an item is clicked
     }
 }
